715491219e848af51d7fdcebd35412a408a0d98d
Brian Yee <bay001@tscc-1-39.sdsc.edu>
2017-08-31 14:54:44 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CLIPSEQ
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='clip_analysis',
+    version='',
+    packages=['clip_analysis', 'legacy'],
+    package_dir={
+        'clip_analysis':'clip_analysis'
+    },
+    include_package_data=True,
+    url='',
+    license='',
+    author='brianyee',
+    author_email='',
+    description='collection of python bioinformatics clip analysis scripts',
+)
